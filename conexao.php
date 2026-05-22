8ce51f64384f49ef2c028870ad0dda906be2b5e6
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-05-22 09:55:45 -0300

    segundo deploy

--- conexao.php
+++ conexao.php
@@ -1,5 +1,10 @@
 <?php
-$conexao = new mysqli("localhost", "root", "1234", "lista_compras");
+$host    = "sql305.infinityfree.com";
+$usuario = "if0_41954082";
+$senha   = "CQOKUWZt26RzxO";
+$banco   = "if0_41954082_lista_compras";
+
+$conexao = new mysqli($host, $usuario, $senha, $banco);
 $conexao->set_charset("utf8mb4");
 
 if ($conexao->connect_error) {
